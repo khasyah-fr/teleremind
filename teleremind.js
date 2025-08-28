6b9838f777374ccbcfd201bd8bff2d7ce874c2ca
fitra.khasyah <fitra.khasyah@ipc.co.id>
2025-08-28 15:03:44 +0700

    feat: initial reminder function

---
+++ teleremind.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const axios = require('axios')
+const TelegramBot = require('node-telegram-bot-api')
+
+const BOT_TOKEN = process.env.BOT_TOKEN
+const CHAT_ID = process.env.CHAT_ID
+const TARGET_URL = process.env.TARGET_URL
+
+const teleBot = new TelegramBot(BOT_TOKEN, {polling: false})
+
+async function checkWeb() {
+    try {
+        const response = await axios.get(TARGET_URL, {timeout: 10000})
+        if ( response.status >= 200 && response.status < 400) {
+            await teleBot.sendMessage(CHAT_ID, `Web ${TARGET_URL} is up`)
+        } else {
+            await teleBot.sendMessage(CHAT_ID, `Web ${TARGET_URL} is down with status ${response.status}`)
+        }
+    } catch (error) {
+        await teleBot.sendMessage(CHAT_ID, `Web ${TARGET_URL} is erroneous with message ${error.message}`)
+    }
+}
+
+checkWeb()
+setInterval(checkWeb, 30 * 60 * 1000)
